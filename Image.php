5d99e8bd569357f74126a5ca8aba79d5e7300ed6
shaikhmiraj <shaikhmiraj2005@gmail.com>
2026-05-18 12:31:24 +0530

    Add files via upload

--- Image.php
+++ Image.php
@@ -1,90 +1,72 @@
 <?php
 /**
- * @package dompdf
- * @link    https://github.com/dompdf/dompdf
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ * @package php-svg-lib
+ * @link    http://github.com/PhenX/php-svg-lib
+ * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
  */
-namespace Dompdf\Renderer;
 
-use Dompdf\Frame;
-use Dompdf\FrameDecorator\Image as ImageFrameDecorator;
-use Dompdf\Image\Cache;
+namespace Svg\Tag;
 
-/**
- * Image renderer
- *
- * @package dompdf
- */
-class Image extends Block
+use Svg\Style;
+
+class Image extends AbstractTag
 {
-    /**
-     * @param ImageFrameDecorator $frame
-     */
-    function render(Frame $frame)
+    protected $x = 0;
+    protected $y = 0;
+    protected $width = 0;
+    protected $height = 0;
+    protected $href = null;
+
+    protected function before($attributes)
     {
-        $style = $frame->get_style();
-        $border_box = $frame->get_border_box();
+        parent::before($attributes);
 
-        $this->_set_opacity($frame->get_opacity($style->opacity));
+        $surface = $this->document->getSurface();
+        $surface->save();
 
-        // Render background & borders
-        $this->_render_background($frame, $border_box);
-        $this->_render_border($frame, $border_box);
-        $this->_render_outline($frame, $border_box);
-
-        $content_box = $frame->get_content_box();
-        [$x, $y, $w, $h] = $content_box;
-
-        $src = $frame->get_image_url();
-        $alt = null;
-
-        if (Cache::is_broken($src) &&
-            $alt = $frame->get_node()->getAttribute("alt")
-        ) {
-            $font = $style->font_family;
-            $size = $style->font_size;
-            $word_spacing = $style->word_spacing;
-            $letter_spacing = $style->letter_spacing;
-
-            $this->_canvas->text(
-                $x,
-                $y,
-                $alt,
-                $font,
-                $size,
-                $style->color,
-                $word_spacing,
-                $letter_spacing
-            );
-        } elseif ($w > 0 && $h > 0) {
-            if ($style->has_border_radius()) {
-                [$tl, $tr, $br, $bl] = $style->resolve_border_radius($border_box, $content_box);
-                $this->_canvas->clipping_roundrectangle($x, $y, $w, $h, $tl, $tr, $br, $bl);
-            }
-
-            $this->_canvas->image($src, $x, $y, $w, $h, $style->image_resolution);
-
-            if ($style->has_border_radius()) {
-                $this->_canvas->clipping_end();
-            }
-        }
-
-        if ($msg = $frame->get_image_msg()) {
-            $parts = preg_split("/\s*\n\s*/", $msg);
-            $font = $style->font_family;
-            $height = 10;
-            $_y = $alt ? $y + $h - count($parts) * $height : $y;
-
-            foreach ($parts as $i => $_part) {
-                $this->_canvas->text($x, $_y + $i * $height, $_part, $font, $height * 0.8, [0.5, 0.5, 0.5]);
-            }
-        }
-
-        $id = $frame->get_node()->getAttribute("id");
-        if (strlen($id) > 0) {
-            $this->_canvas->add_named_dest($id);
-        }
-
-        $this->debugBlockLayout($frame, "blue");
+        $this->applyTransform($attributes);
     }
-}
+
+    public function start($attributes)
+    {
+        $height = $this->document->getHeight();
+        $width = $this->document->getWidth();
+        $this->y = $height;
+
+        if (isset($attributes['x'])) {
+            $this->x = $this->convertSize($attributes['x'], $width);
+        }
+        if (isset($attributes['y'])) {
+            $this->y = $height - $this->convertSize($attributes['y'], $height);
+        }
+
+        if (isset($attributes['width'])) {
+            $this->width = $this->convertSize($attributes['width'], $width);
+        }
+        if (isset($attributes['height'])) {
+            $this->height = $this->convertSize($attributes['height'], $height);
+        }
+
+        if (isset($attributes['xlink:href'])) {
+            $this->href = $attributes['xlink:href'];
+        }
+
+        if (isset($attributes['href'])) {
+            $this->href = $attributes['href'];
+        }
+
+        $this->document->getSurface()->transform(1, 0, 0, -1, 0, $height);
+
+        if (\strtolower(\substr($this->href, 0, 7)) === "phar://" || ($this->document->allowExternalReferences === false && \strtolower(\substr($this->href, 0, 5) !== "data:"))) {
+            return;
+        }
+
+        $this->document->getSurface()->drawImage($this->href, $this->x, $this->y, $this->width, $this->height);
+    }
+
+    protected function after()
+    {
+        $this->document->getSurface()->restore();
+    }
+} 
