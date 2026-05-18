396841d56a4a0ac9d2abdb610cdc40362fa87f38
shaikhmiraj <shaikhmiraj2005@gmail.com>
2026-05-18 12:05:36 +0530

    Add files via upload

---
+++ change_password.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+require_once "config.php";
+
+if(!isset($_SESSION['user_id'])){
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Change Password</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="navbar">
+    <a href="php/dashboard.php">Dashboard</a>
+    <a href="profile.php">Profile</a>
+    <a href="login.php">Logout</a>
+</div>
+
+<h2 style="text-align:center; margin-top:40px;">Change Password</h2>
+
+<div class="container">
+
+<form action="php/update_password.php" method="POST">
+
+<div class="form-group">
+    <div class="password-wrapper">
+        <input type="password"
+               id="old_password"
+               name="old_password"
+               class="form-control"
+               placeholder="Enter old password"
+               required>
+
+        <span class="toggle-password"
+              onclick="togglePassword('old_password', this)">
+            <i class="fa-solid fa-eye-slash"></i>
+        </span>
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="password-wrapper">
+        <input type="password"
+               id="new_password"
+               name="new_password"
+               class="form-control"
+               placeholder="Enter new password"
+               required>
+
+        <span class="toggle-password"
+              onclick="togglePassword('new_password', this)">
+            <i class="fa-solid fa-eye-slash"></i>
+        </span>
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="password-wrapper">
+        <input type="password"
+               id="confirm_password"
+               name="confirm_password"
+               class="form-control"
+               placeholder="Confirm new password"
+               required>
+
+        <span class="toggle-password"
+              onclick="togglePassword('confirm_password', this)">
+            <i class="fa-solid fa-eye-slash"></i>
+        </span>
+    </div>
+</div>
+
+<button type="submit" class="btn-primary">
+        Update Password
+    </button>
+
+</form>
+
+</div>
+
+<script>
+function togglePassword(fieldId, element) {
+
+    var input = document.getElementById(fieldId);
+    var icon = element.querySelector("i");
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    }
+}
+</script>
+
+</body>
+</html>
