8d27708a2a95c4474b2cfebdb8542e4bccefaada
shaikhmiraj <shaikhmiraj2005@gmail.com>
2026-05-18 12:25:07 +0530

    Add files via upload

--- Exception.php
+++ Exception.php
@@ -1,27 +1,10 @@
 <?php
-/**
- * @package dompdf
- * @link    https://github.com/dompdf/dompdf
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
-namespace Dompdf;
+
+namespace Masterminds\HTML5;
 
 /**
- * Standard exception thrown by DOMPDF classes
- *
- * @package dompdf
+ * The base exception for the HTML5 project.
  */
 class Exception extends \Exception
 {
-
-    /**
-     * Class constructor
-     *
-     * @param string $message Error message
-     * @param int $code       Error code
-     */
-    public function __construct($message = null, $code = 0)
-    {
-        parent::__construct($message, $code);
-    }
 }
